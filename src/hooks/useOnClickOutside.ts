b34dda41017f6bf6c72e8655c82f7521c074cdd0
ameerjako <rematchlop@gmail.com>
2025-06-26 21:41:14 +0300

    added search

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,27 @@
+import { useEffect, RefObject } from 'react';
+
+/**
+ * Hook that alerts when you click outside of the passed ref
+ */
+export const useOnClickOutside = (
+  ref: RefObject<HTMLElement>,
+  handler: (event: MouseEvent | TouchEvent) => void
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}; 
