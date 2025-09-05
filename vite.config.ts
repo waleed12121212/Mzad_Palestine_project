fd98c0bc0cdf8226fbb41173dd89588a864f0da5
waleed12121212 <waleeddweikat67@gmail.com>
2025-09-05 22:54:35 +0300

    add https

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const API_URL = process.env.VITE_API_URL || 'http://mazadpalestine.runasp.net';
+const API_URL = process.env.VITE_API_URL || 'https://mazadpalestine.runasp.net';
 
 // Add base URL configuration for production
 const base = process.env.NODE_ENV === 'production' ? '/' : '/';
